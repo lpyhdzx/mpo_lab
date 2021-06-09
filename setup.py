1d4be9381e2c4f549a7711b7a0b918a242acda1b
liupeiyu <506302360@qq.com>
2021-06-09 17:18:22 +0800

    init package

---
+++ setup.py
@@ -0,0 +1,25 @@
+from __future__ import print_function
+from setuptools import setup, find_packages
+from Cython.Build import cythonize
+from distutils.core import Extension
+
+module1 = Extension('mpo_lab.MPOtorch',['mpo_lab/MPOtorch.c'])
+module2 = Extension('mpo_lab.Matrix2MPO',['mpo_lab/Matrix2MPO.c'])
+
+setup(
+    name="mpo_lab",
+    version="0.1.0",
+    author="Peiyu Liu, Zefeng Gao",
+    author_email="liupeiyustu@163.com",
+    description="Tools for model compression based on methods from paper, 'Enabling Lightweight Fine-tuning for Pre-trained Language Model Compression based on Matrix Product Operators'",
+    license="MIT",
+    url="https://arxiv.org/abs/2106.02205", # paper url
+    packages=find_packages(exclude=('*.c')),
+    ext_modules=[module1,module2],
+    include_package_data=True,
+    install_requires=[
+        'numpy>=1.19.2',
+        'torch>=1.8.0'
+    ],
+    zip_safe=True
+)
