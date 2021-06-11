483a9a341d475ffea2cce9211d0dbff33a25e5a9
liupeiyu <506302360@qq.com>
2021-06-11 22:02:50 +0800

    up, add github URL to setup.py

--- setup.py
+++ setup.py
@@ -8,12 +8,12 @@ module2 = Extension('mpo_lab.Matrix2MPO',['mpo_lab/Matrix2MPO.c'])
 
 setup(
     name="mpo_lab",
-    version="0.1.0",
+    version="0.1.1",
     author="Peiyu Liu, Zefeng Gao",
     author_email="liupeiyustu@163.com",
     description="Tools for model compression based on methods from paper, 'Enabling Lightweight Fine-tuning for Pre-trained Language Model Compression based on Matrix Product Operators'",
     license="MIT",
-    url="https://arxiv.org/abs/2106.02205", # paper url
+    url="https://github.com/lpyhdzx/mpo_lab", # paper url
     packages=find_packages(),
     ext_modules=[module1,module2],
     include_package_data=True,
